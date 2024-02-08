b3f86ef02fe754c604a107bc266ff07ec162f976
Gokul Suresh S.V <142083083+Gokulsuresh1918@users.noreply.github.com>
2024-02-08 11:09:18 +0530

    Time: 67 ms (43.03%) | Memory: 50.2 MB (13.76%) - LeetSync

--- 2741-function-composition/function-composition.js
+++ 2741-function-composition/function-composition.js
@@ -3,9 +3,7 @@
  * @return {Function}
  */
 var compose = function (functions) {
-    if (functions.length === 0) {
-        return function (x) { return x }
-    }
+ 
     return function (x) {
         let a = x
         for (let i = functions.length - 1; i >= 0; i--) {
