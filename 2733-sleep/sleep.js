9e4508a5d382c067aadfd5953dacc843db1fcba6
Gokul Suresh S.V <142083083+Gokulsuresh1918@users.noreply.github.com>
2024-02-09 11:09:03 +0530

    Time: 51 ms (69.94%) | Memory: 48.9 MB (14.71%) - LeetSync

--- 2733-sleep/sleep.js
+++ 2733-sleep/sleep.js
@@ -3,11 +3,7 @@
  * @return {Promise}
  */
 async function sleep(millis) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve()
-        },millis)
-    })
+    await new Promise(resolve => setTimeout(resolve, millis))
 }
 
 /** 
