ebc71f46661a72cb8e3c87ebcbd124b800882d4c
Gokul Suresh S.V <142083083+Gokulsuresh1918@users.noreply.github.com>
2024-02-17 12:15:49 +0530

    Time: 78 ms (44.06%) | Memory: 55.6 MB (61.81%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.ts
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.ts
@@ -1,25 +1,21 @@
-/**
- * @param {string[]} words
- * @return {string}
- */
-const checking = (word: string): boolean => {
-    let first = 0;
-    let last = word.length - 1;
-    while (first < last) {
-        if (word.charAt(first) !== word.charAt(last)) {
-            return false;
-        }
-        first += 1;
-        last -= 1;
-    }
-    return true;
-};
-
 function firstPalindrome(words: string[]): string {
-    for (let i = 0; i < words.length; i++) {
-        if (checking(words[i])) {
-            return words[i];
+    for(let word of words) {
+        if(isPalindrome(word)) {
+            return word
         }
     }
-    return '';
-}
+    return ""
+
+
+
+    function isPalindrome(str: string): boolean {
+        let start = 0
+        let end = str.length - 1
+        while(start < end) {
+            if(str[start] != str[end]) return false
+            start++
+            end--
+        }
+        return true
+    }
+};
