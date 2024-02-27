ac2fd946b1846e73cc637d7ba78da34bc789bfe0
Gokul Suresh S.V <142083083+Gokulsuresh1918@users.noreply.github.com>
2024-02-27 09:56:58 +0530

    Time: 58 ms (52.06%) | Memory: 51.5 MB (39.68%) - LeetSync

---
+++ 100-same-tree/same-tree.ts
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    if (!p && !q) {
+        return true
+    }
+    if (!p || !q || p.val !== q.val) {
+        return false
+    }
+   return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+};
