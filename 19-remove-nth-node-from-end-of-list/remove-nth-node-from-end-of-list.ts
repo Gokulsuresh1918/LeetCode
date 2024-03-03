fe88cdf5e870f670316e408db991cf6ad1aae768
Gokul Suresh S.V <142083083+Gokulsuresh1918@users.noreply.github.com>
2024-03-03 16:57:49 +0530

    Time: 62 ms (49.62%) | Memory: 51.5 MB (63.71%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.ts
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.ts
@@ -10,11 +10,36 @@
  * }
  */
 
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
 function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
-       let fast = head, slow = head
-    for (let i = 0; i < n; i++) fast = fast.next
-    if (!fast) return head.next
-    while (fast.next) fast = fast.next, slow = slow.next
-    slow.next = slow.next.next
-    return head
-};
+    if (head == null) {
+        return head;
+    }
+    let count: number = 1;
+    let current: ListNode | null = head;
+    while (current.next!= null) {
+        count++;
+        current = current.next;
+    }
+    let index: number = count - n;
+    if (index === 0) {
+        return head.next;
+    }
+    current = head;
+    for (let i: number = 0; i < index - 1; i++) {
+        current = current.next;
+    }
+    current.next = current.next.next;
+    return head;
+}
