d0b26cc0302481e311335c122c17a102079ac86c
Gokul Suresh S.V <142083083+Gokulsuresh1918@users.noreply.github.com>
2024-03-07 09:57:56 +0530

    Time: 64 ms (17.46%) | Memory: 51.3 MB (49.59%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.ts
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.ts
@@ -13,7 +13,7 @@
 function middleNode(head: ListNode | null): ListNode | null {
  let fast =head
  let slow = head;
-    while (fast && fast.next) {
+    while (fast?.next) {
         fast = fast.next.next;
         slow = slow.next;
     }
