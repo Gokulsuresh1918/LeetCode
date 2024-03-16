1340a5a156a632d267898024f0a9c571cc5e8640
Gokul Suresh S.V <142083083+Gokulsuresh1918@users.noreply.github.com>
2024-03-16 20:37:07 +0530

    Time: 86 ms (74.03%) | Memory: 56.9 MB (69.26%) - LeetSync

--- 525-contiguous-array/contiguous-array.js
+++ 525-contiguous-array/contiguous-array.js
@@ -2,31 +2,20 @@
  * @param {number[]} nums
  * @return {number}
  */
-const findMaxLength = (nums) => {
-  const hash = {};
-  let max_length = 0;
-  let count = 0;
-  for (let i = 0; i < nums.length; i++) {
-    const current = nums[i];
-    if (current === 0) {
-        // if the current element is 0, then we decrement the count
-      count--;
-    } else if (current === 1) {
-        // if the current element is 1, then we increment the count
-      count++;
+var findMaxLength = function(nums) {
+    const map = new Map();
+    map.set(0, -1);
+    let maxLength = 0;
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        count += nums[i] === 1 ? 1 : -1;
+        if (map.has(count)) {
+            maxLength = Math.max(maxLength, i - map.get(count));
+        } else {
+            map.set(count, i);
+        }
     }
 
-    if (count === 0) {
-        // if the count is equal to o then we have a contiguous subarray of length equal to i+1
-      max_length = i + 1;
-    }
-    if (count in hash) {
-    
-      max_length = Math.max(max_length, i - hash[count]); // update our max length
-    } else {
-      hash[count] = i;
-    }
-
-  }
-  return max_length;
+    return maxLength;
 };
