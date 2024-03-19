bee48569e20164f5e5b8711fc6518976491327c0
Gokul Suresh S.V <142083083+Gokulsuresh1918@users.noreply.github.com>
2024-03-19 10:18:27 +0530

    Time: 69 ms (94.47%) | Memory: 52.8 MB (85.68%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.js
@@ -0,0 +1,30 @@
+/**
+ * @param {array[]} tasks
+ * @param {number} n
+ * @return {number}
+ */
+var leastInterval = function(tasks, n) {
+  // the map will be our tracking mechanism
+  let m = new Map();
+  
+  // the max occurrences
+  let maxVal = 0;
+  
+  // the number of tasks that has the max occurrences
+  let maxValCount = 0;
+  
+  for(let k of tasks){
+    let tVal = m.has(k) ? m.get(k)+1: 1;
+    m.set(k, tVal);
+	// set our maxVal and number of maxVal tasks only if we have a new max
+    if(tVal > maxVal){
+      maxVal = tVal;
+      maxValCount = 1;
+	// otherwise, increment number of maxVal tasks
+    } else if(tVal === maxVal){
+      maxValCount++;
+    }
+  }
+  // our formula, handle the edge case
+  return Math.max(tasks.length, (maxVal - 1) * (n + 1) + maxValCount);
+};
