771b69aa21faf7da5ec970ccd602aa8e3081ae41
Gokul Suresh S.V <142083083+Gokulsuresh1918@users.noreply.github.com>
2024-06-18 21:35:22 +0530

    Time: 2127 ms (5.56%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,3 @@
+select distinct product_id, 10 as price from Products where product_id not in(select distinct product_id from Products where change_date <='2019-08-16' )
+union 
+select product_id, new_price as price from Products where (product_id,change_date) in (select product_id , max(change_date) as date from Products where change_date <='2019-08-16' group by product_id)
