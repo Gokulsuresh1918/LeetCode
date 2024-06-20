1fa8a9afe32c99d4166bbb6b13d992745a861f0c
Gokul Suresh S.V <142083083+Gokulsuresh1918@users.noreply.github.com>
2024-06-20 09:23:17 +0530

    Time: 1695 ms (86.37%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -0,0 +1,15 @@
+SELECT "Low Salary" AS category,
+       sum(income < 20000) AS accounts_count
+  FROM Accounts
+
+UNION
+
+SELECT "Average Salary" AS category,
+       sum(income BETWEEN 20000 AND 50000) AS accounts_count
+  FROM Accounts
+
+UNION
+
+SELECT "High Salary" AS category,
+       sum(income > 50000) AS accounts_count
+  FROM Accounts;
