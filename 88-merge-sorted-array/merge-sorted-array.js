5f55a9471ae69ba0130fd4e12c8c34134513fd7a
Gokul Suresh S.V <142083083+Gokulsuresh1918@users.noreply.github.com>
2024-08-11 01:49:49 +0530

    Time: 55 ms (47.43%) | Memory: 49.1 MB (46.50%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+
+    let fir = m - 1
+    let sec = n - 1
+    let tot = m + n - 1
+    while (fir >= 0 && sec >= 0) {
+        if (nums1[fir] > nums2[sec]) {
+            nums1[tot] = nums1[fir]
+            fir--
+        } else {
+            nums1[tot] = nums2[sec]
+            sec--
+        }
+        tot--
+    }
+    while (sec >= 0) {
+        nums1[tot] = nums2[sec]
+        sec--
+        tot--
+    }
+};
