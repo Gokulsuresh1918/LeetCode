ae8847ffe8773bf9fea6f646889aeef6a1cf915d
Gokul Suresh S.V <142083083+Gokulsuresh1918@users.noreply.github.com>
2024-08-12 18:20:34 +0530

    Time: 42 ms (93.45%) | Memory: 49.2 MB (8.52%) - LeetSync

---
+++ 2556-convert-the-temperature/convert-the-temperature.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} celsius
+ * @return {number[]}
+ */
+var convertTemperature = function (celsius) {
+    
+    let kelvin = celsius + 273.15
+    let Fahrenheit = celsius * 1.80 + 32.00
+    let ans=[]
+    ans.push(kelvin)
+    ans.push(Fahrenheit)
+    return ans
+};
