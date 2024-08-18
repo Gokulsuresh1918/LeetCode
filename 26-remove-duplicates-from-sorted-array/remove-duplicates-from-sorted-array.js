e298669eb36b1ca16f6bbfa9c9501fe3ad26682d
Gokul Suresh S.V <142083083+Gokulsuresh1918@users.noreply.github.com>
2024-08-18 22:52:55 +0530

    Time: 64 ms (59.21%) | Memory: 52.4 MB (43.71%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -2,13 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
-    let i=1
-    for(let j=1 ;j<nums.length;j++){
-        if(nums[j] !=nums[i-1]){
-            nums[i] = nums[j]
-            i++
+var removeDuplicates = function (nums) {
+    let l = 1
+    for (let r = 1; r < nums.length; r++) {
+        if (nums[r] !== nums[r - 1]) {
+            nums[l] = nums[r]
+            l++
         }
     }
-    return i;
+    return l
 };
