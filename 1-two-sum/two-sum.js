07035866b93aa76aebc3a83584d9f4670e4a2fd9
Gokul Suresh S.V <142083083+Gokulsuresh1918@users.noreply.github.com>
2024-12-19 17:22:13 +0530

    Time: 80 ms (13.34%) | Memory: 49.3 MB (91.20%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,11 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    for (let i = 0; i <= nums.length; i++) {
-        for (let j = i + 1; j <= nums.length; j++) {
-            if (nums[i] + nums[j] == target) {
-                return [i,j]
-            }
+
+for(i=0;i<=nums.length-1;i++){
+    for(j=i+1;j<=nums.length-1;j++){
+        if(nums[i]+nums[j]==target){
+            return [i,j]
         }
     }
+}
 };
