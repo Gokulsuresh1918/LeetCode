5abf69c51a774e835b98f3ca684927ad9f34d824
Gokul Suresh S.V <142083083+Gokulsuresh1918@users.noreply.github.com>
2025-01-28 22:27:30 +0530

    Time: 74 ms (9.02%) | Memory: 59.5 MB (95.32%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,10 +3,8 @@
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
-     nums.sort((a,b)=>a-b)
-
-    for (let i = 0; i < nums.length-1; i++) {
-
+    nums.sort((a, b) => b-a)
+    for (let i = 0; i < nums.length - 1; i++) {
         if (nums[i] === nums[i + 1]) {
             return true
         }
