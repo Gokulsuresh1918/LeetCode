fc7322d29d8490e5a19089e2bb389d96380b4554
Gokul Suresh S.V <142083083+Gokulsuresh1918@users.noreply.github.com>
2025-02-01 16:28:50 +0530

    Time: 51 ms (21.86%) | Memory: 64.2 MB (27.93%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -2,14 +2,14 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function(strs) {
-    let ans={}
-    for(s of strs){
-        let key=s.split('').sort().join('')
-              if(!ans[key]){
-                ans[key]=[]
-              }
-              ans[key].push(s)
+var groupAnagrams = function (strs) {
+    let ans = {}
+    for (let a of strs) {
+        let key = a.split('').sort().join('')
+        if (!ans[key]) {
+            ans[key] = []
+        }
+        ans[key].push(a)
     }
     return Object.values(ans)
 };
