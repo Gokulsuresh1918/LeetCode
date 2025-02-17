d6b6faa3b4daba79757aa968ea00906ccd2f4790
Gokul Suresh S.V <142083083+Gokulsuresh1918@users.noreply.github.com>
2025-02-17 17:47:32 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (79.24%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.ts
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxDepth(root: TreeNode | null): number {
+    if(root===null){return 0} 
+     const a = maxDepth(root.right)
+     const b= maxDepth(root.left)
+     return Math.max(a,b)+1
+  };
