68c78bfc3288d96e06ed63f212d40f0ec0f56d34
Gokul Suresh S.V <142083083+Gokulsuresh1918@users.noreply.github.com>
2025-06-20 21:29:49 +0530

    Time: 28 ms (40.80%) | Memory: 56.6 MB (63.31%) - LeetSync

---
+++ 1-two-sum/two-sum.ts
@@ -0,0 +1,10 @@
+function twoSum(nums: number[], target: number): number[] {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] === target - nums[i]) {
+                return [i, j]
+            }
+        }
+    }
+    return []
+};
