7ba87ad39ad9ea61911a97093c031fef6c265d83
Gokul Suresh S.V <142083083+Gokulsuresh1918@users.noreply.github.com>
2025-06-23 11:13:56 +0530

    Time: 0 ms (100.00%) | Memory: 55.5 MB (91.74%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -1,10 +1,12 @@
 function twoSum(nums: number[], target: number): number[] {
+
+    const map = new Map<number, number>();
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[j] === target - nums[i]) {
-                return [i, j]
-            }
+        let diff = target - nums[i]
+        if (map.has(diff)) {
+            return [map.get(diff)!, i]
         }
+        map.set(nums[i],i)
     }
-    return []
 };
+
